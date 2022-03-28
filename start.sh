dc7fa70daf396e297c8e5a58db5339bfd43f166f
aref-mehran <aref.mehran.w@gmail.com>
2022-03-28 12:05:45 +0430

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 cd /home
-caddy run &
-
 
 mkdir ./projects
 cd projects
 git clone --depth=1  https://github.com/aref-mehran/english10.git
+
+git clone --depth=1  https://github.com/aref-mehran/arvan-pizan-docker.git
+cd arvan-pizan-docker
+caddy run &
+
 code-server --port 8000
+
+
 
