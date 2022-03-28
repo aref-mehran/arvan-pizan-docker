770129d53a7b9384fcf0a25a77e3167ec1bea6bb
aref-mehran <aref.mehran.w@gmail.com>
2022-03-28 12:09:41 +0430

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,6 +7,8 @@ git clone --depth=1  https://github.com/aref-mehran/english10.git
 
 git clone --depth=1  https://github.com/aref-mehran/arvan-pizan-docker.git
 cd arvan-pizan-docker
+chmod +x ./run.sh
+./run.sh &
 caddy run &
 
 code-server --port 8000
