b9807d053cbf65a95aa73935e8b75d974fcc7844
Your Name <you@example.com>
2022-03-30 07:53:33 +0000

    github workflow create dist folder. so caddy should change

--- run.sh
+++ run.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 echo run.sh started ....
+
+cd /home
+mkdir ./projects
+cd projects
+cd arvan-pizan-docker
+caddy run &
+
+git clone --depth=1 -b dist  https://github.com/aref-mehran/english10.git
+git clone --depth=1 -b dist https://github.com/aref-mehran/ionic-react-music-app.git
+
+
 cd /home/projects
 mkdir .well-known 
-cp /home/projects/english10/src/assetlinks.json .well-known/
+cp /home/projects/english10/assetlinks.json .well-known/
+
+
+code-server --port 8000 
+
+
--- start.sh
+++ start.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 cd /home
-
 mkdir ./projects
 cd projects
-git clone --depth=1  https://github.com/aref-mehran/english10.git
-
 git clone --depth=1  https://github.com/aref-mehran/arvan-pizan-docker.git
 cd arvan-pizan-docker
 chmod +x ./run.sh
 ./run.sh &
-caddy run &
-
-code-server --port 8000
-
 
 
