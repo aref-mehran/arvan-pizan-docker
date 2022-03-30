c67b9ae3e850ada621c5acd1ba74e735f6f13f51
Your Name <you@example.com>
2022-03-30 08:12:36 +0000

    pull every 300 sec

--- run.sh
+++ run.sh
@@ -4,13 +4,21 @@ echo run.sh started ....
 cd /home/projects
 
 git clone --depth=1 -b dist https://github.com/aref-mehran/ionic-react-music-app.git
+cd ionic-react-music-app
+watch -n 300 git pull &
 
-cd /home/projects/arvan-pizan-docker
-caddy run &
+git clone --depth=1 -b dist https://github.com/aref-mehran/english10.git
+cd english10
+watch -n 300 git pull &
 
 cd /home/projects
 mkdir .well-known 
 cp /home/projects/english10/assetlinks.json .well-known/
+
+
+cd /home/projects/arvan-pizan-docker
+caddy run &
+
 
 
 code-server --port 8000 
