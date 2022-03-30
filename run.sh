1d04892576f71a2ebc48155366d5885768fcaebc
Your Name <you@example.com>
2022-03-30 08:00:31 +0000

    changes

--- run.sh
+++ run.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 echo run.sh started ....
 
-cd /home
-mkdir ./projects
-cd projects
-cd arvan-pizan-docker
-caddy run &
+cd /home/projects
+
 
 git clone --depth=1 -b dist  https://github.com/aref-mehran/english10.git
 git clone --depth=1 -b dist https://github.com/aref-mehran/ionic-react-music-app.git
@@ -15,6 +12,8 @@ cd /home/projects
 mkdir .well-known 
 cp /home/projects/english10/assetlinks.json .well-known/
 
+cd /home/projects/arvan-pizan-docker
+caddy run &
 
 code-server --port 8000 
 
