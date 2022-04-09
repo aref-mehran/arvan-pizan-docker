71815106a73717e335e19180a8d925253e4e2bdb
aref-mehran <aref.mehran.w@gmail.com>
2022-04-09 14:22:28 +0430

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,8 +7,6 @@ git clone --depth=1 -b dist https://github.com/aref-mehran/ionic-react-music-app
 cd /home/projects/ionic-react-music-app
 watch -n 300 git pull &
 
-cd /home/projects/
-git clone --depth=1 -b dist https://github.com/aref-mehran/english10.git
 cd /home/projects/english10
 watch -n 300 git pull &
 
